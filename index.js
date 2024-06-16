65b841ee8985a17ca0bdc23c3e1769aa4a2ed32f
geekcoldhand <horatiousaharris@gmail.com>
2024-06-15 21:12:54 -0400

    ready for backend

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+let textResult = "";
 const outputDiv = document.getElementById("output");
 const recognition = new webkitSpeechRecognition() || new SpeechRecognition();
+const synth = window.speechSynthesis;
+
 const stopButton = document
   .getElementById("stop-btn")
-  .addEventListener("onclick", handleStopButtonClick(), false);
+  .addEventListener("click", handleStopButtonClick, false);
 const startButton = document
   .getElementById("start-btn")
-  .addEventListener("click", handleStartButtonClick, false);
-// recognition.interimResults = true;
-// recognition.continuous = true;
+  .addEventListener("click", handleStartButtonClick, false); //TODO: this click need to be socket connection
+recognition.interimResults = true;
+recognition.continuous = true;
 
 function handleStartButtonClick() {
   console.log("Start button clicked.");
@@ -17,12 +20,17 @@ function handleStartButtonClick() {
 }
 function handleStopButtonClick() {
   console.log("Stop button clicked.");
-  outputDiv.textContent = "stopped...";
+  recognition.stop();
+  const utterThis = new SpeechSynthesisUtterance(textResult);
+  synth.speak(utterThis);
+  //outputDiv.textContent = "stopped...";
 }
 
 recognition.onresult = (event) => {
   const result = event.results[event.results.length - 1][0].transcript;
+  textResult = result;
   outputDiv.textContent = result;
+  //TODO: Send result to server
 };
 
 recognition.onend = () => {
